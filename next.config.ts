5452343d61213f534c560e0ca41adf8498e1b12b
Vanderbot <vanderbot@example.com>
2026-06-18 09:55:29 +0000

    Fix basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  assetPrefix: '/vanderbot-mobile',
+  basePath: '/vanderbot-mobile',
   images: {
     unoptimized: true,
   },
